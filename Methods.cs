8a797e6c53fe585aa20452a6cdc9de8a53bc2694
AbdullayevTuran-027 <99797460+AbdullayevTuran-027@users.noreply.github.com>
2022-03-09 21:30:58 -0800

    123
    321

---
+++ Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace mehods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            //string str = "Xacmaz";
+            //string reverse = "";
+            //int length = str.Length - 1;
+
+            //while (length >= 0)
+            //{
+            //    reverse += str[length];
+            //    length--;
+            //}
+
+            //Console.WriteLine(reverse);
+
+
+            //int[] arr = { 11, -17, 30, -25, 44, -32, -98 };
+            //int loop = 0;
+
+            //for (loop = 0; loop < arr.Length; loop++)
+            //{
+            //    if (arr[loop] < 0)
+            //        arr[loop] = -arr[loop];
+            //}
+
+            //Console.WriteLine("Array elements:");
+            //for (loop = 0; loop < arr.Length; loop++)
+            //{
+            //    Console.Write(arr[loop] + " ");
+                #endregion
+            }
+        }
+    }
+}
